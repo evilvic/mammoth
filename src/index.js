7f13ae35b68f085b5afc36037d97270e0e3b9420
Víctor Peña Romero <vic.pero@icloud.com>
2020-06-21 15:02:04 -0500

    add styled-components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import GlobalStyles from './styles/globalStyles'
+
 const DummyComponent = () => <h1>Hey!</h1>
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles/>
     <DummyComponent/>
   </React.StrictMode>,
   document.getElementById('root')
